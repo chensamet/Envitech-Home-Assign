d7f41ba88a7f9b5673df173f946cd5c1af2d4875
chensamet <chensamet123@gmail.com>
2023-01-25 09:52:19 +0200

    Cleaning code

--- client/src/components/Legend.js
+++ client/src/components/Legend.js
@@ -6,13 +6,13 @@ const Legend = (props) => {
   return (
     <Fragment>
       <h2>Legend</h2>
-        <ul class="lagent">
-          {legend?.map((legend) => {
-            return legend.tags.map((tag) => {
-              return <li style={{ color: tag.Color }}>{tag.Label}</li>;
-            });
-          })}
-        </ul>
+      <ul class="lagent">
+        {legend?.map((legend) => {
+          return legend.tags.map((tag) => {
+            return <li style={{ color: tag.Color }}>{tag.Label}</li>;
+          });
+        })}
+      </ul>
     </Fragment>
   );
 };
